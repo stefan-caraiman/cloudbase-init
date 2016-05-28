f979b4f22ea8fcd6d09a3f93dec5ffb835eed87c
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-05-28 16:32:38 +0300

    Add a new layer of abstractization over the network metadata

--- cloudbaseinit/metadata/services/base.py
+++ cloudbaseinit/metadata/services/base.py
@@ -19,6 +19,7 @@ import gzip
 import io
 import time
 
+import ipaddress
 from oslo_config import cfg
 from oslo_log import log as oslo_logging
 import six
@@ -40,6 +41,27 @@ CONF.register_opts(opts)
 
 LOG = oslo_logging.getLogger(__name__)
 
+# Network types
+IPV4 = 4
+IPV6 = 6
+
+# Field names related to network configuration
+BROADCAST = "broadcast"
+DNS = "dns_nameservers"
+GATEWAY = "gateway"
+ID = "id"
+IP_ADDRESS = "ip_address"
+NAME = "name"
+MAC_ADDRESS = "mac_address"
+MTU = "mtu"
+NETMASK = "netmask"
+TYPE = "type"
+VERSION = "version"
+
+# Fields name
+LINK = "link"
+NETWORK = "network"
+
 # Both the custom service(s) and the networking plugin
 # should know about the entries of these kind of objects.
 NetworkDetails = collections.namedtuple(
@@ -57,6 +79,236 @@ NetworkDetails = collections.namedtuple(
         "dnsnameservers",
     ]
 )
+
+
+class Field(collections.namedtuple("Field", "name alias default required")):
+
+    """Namedtuple used for fields information."""
+
+    __slots__ = ()
+
+    def __new__(cls, name, alias=None, default=None, required=False):
+        return super(Field, cls).__new__(cls, name, alias, default, required)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseNetworkAdapter(object):
+
+    """Translate the network information from the service specific
+    format to a format that can be easily procesed by cloudbase-init
+    plugins.
+    """
+
+    FIELDS = {
+        LINK: {
+            NAME: Field(name=NAME, required=True),
+            MAC_ADDRESS: Field(name=MAC_ADDRESS, required=True),
+        },
+        NETWORK: {
+            IP_ADDRESS: Field(name=IP_ADDRESS, required=True),
+            NETMASK: Field(name=NETMASK, required=True),
+            GATEWAY: Field(name=GATEWAY, required=True),
+            VERSION: Field(name=VERSION, default=4, required=False),
+            DNS: Field(name=DNS, default=[], required=False),
+        },
+    }
+
+    def __init__(self, service):
+        self._service = service
+        self._fields = {}
+
+        mro = type(self).mro()
+        while mro:
+            parent = mro.pop()
+            try:
+                fields = getattr(parent, "FIELDS")
+                self._fields.update(fields)
+            except AttributeError:
+                pass
+
+    @property
+    def fields(self):
+        """Return the information regarding network data fields."""
+        return self._fields
+
+    @staticmethod
+    def _digest_interface(address, netmask=None):
+        """Compute the provided information."""
+        if netmask:
+            address = six.u("%s/%s" % (address, netmask))
+
+        interface = ipaddress.ip_interface(six.u(address))
+        return {
+            BROADCAST: str(interface.network.broadcast_address),
+            NETMASK: str(interface.netmask),
+            IP_ADDRESS: str(interface.ip),
+            VERSION: str(interface.version)
+        }
+
+    @staticmethod
+    def _get_field(field, raw_data):
+        """Find the required information in the raw data."""
+        aliases = [field.name]
+        if isinstance(field.alias, six.string_types):
+            aliases = aliases.append(field.alias)
+        elif isinstance(field.alias, (list, tuple)):
+            aliases.extend(field.alias)
+
+        for alias in aliases:
+            if alias in raw_data:
+                return field.name, raw_data[alias]
+
+        if not field.required:
+            return field.name, field.default
+
+        raise ValueError("The required field %r is missing." % field.name)
+
+    def get_fields(self, fields, raw_data):
+        """Get the information related to all the received fields
+        if it is posible.
+        """
+        data = {}
+        for field in fields:
+            try:
+                field_name, field_value = self._get_field(raw_data, field)
+                data[field_name] = field_value
+            except ValueError as reason:
+                LOG.warning("Failed to process %r: %s", raw_data, reason)
+                return
+        return data
+
+    @abc.abstractmethod
+    def get_link(self, name):
+        """Return all the available information regarding the received
+        link name.
+
+        :rtype: dict
+
+        .. notes:
+            The returned dictionary should contain al least the following
+            keys: `name` and `mac_address`.
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_links(self):
+        """Return a list with the names of the available links.
+
+        :rtype: list
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_network(self, link, name):
+        """Return all the available information regarding the required
+        network.
+
+        :param link: The name of the required link
+        :type link:  str
+        :param name: The name of the required network
+        :type name: str
+
+        .. notes:
+            The returned dictionary should contain al least the following
+            keys: `name`, `version`, `ip_address`, `netmask` and
+            `dns_nameservers`.
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_networks(self, link):
+        """Returns all the network names bound by the required link.
+
+        :param link: The name of the required link
+        :type link:  str
+
+        :rtype: list
+        """
+        pass
+
+
+class NetworkConfig(object):
+
+    """Process information related to the network."""
+
+    def __init__(self, network_adapter):
+        self._adapter = network_adapter
+        self._networks = []
+
+    def _get_networks(self):
+        """Get all the information available on the current service."""
+        for link_name in self._adapter.get_links():
+            link = self._adapter.get_link(link_name)
+            for network_name in self._adapter.get_networks(link):
+                network = self._adapter.get_network(link_name, network_name)
+                if link and network:
+                    yield link, network
+
+    def _digest(self):
+        """Return a dictionary that contains all the information
+        related to the networks available on the current metadata
+        service.
+
+        .. notes:
+            The structure of the returned dictionary is the following:
+                {
+                    "interface0" : {
+                        "name": "interface0",
+                        "mac_address": "a0:36:9f:2c:e8:80",
+                        "network": {
+                            "ipv4": {
+                                "id": "private-ipv4",
+                                "type": "ipv4",
+                                "ip_address": "10.184.0.244",
+                                "netmask": "255.255.240.0",
+                                "dns_nameservers": [
+                                    "69.20.0.164",
+                                    "69.20.0.196"
+                                ],
+                                # ...
+                            },
+                            "ipv6": {
+                                "id": "private-ipv4",
+                                "type": "ipv6",
+                                "ip_address": "2001:cdba::3257:9652/24"
+                                # ...
+                            }
+                        }
+                    }
+                }
+        """
+        raw_data = {}
+        for link, network in self._get_networks():
+            link_data = raw_data.setdefault(link[NAME], link)
+            network_data = link_data.setdefault("network", {})
+            network_data[network[VERSION]] = network
+
+        return raw_data
+
+    def get_network_details(self):
+        """Return a list of `NetworkDetails` objects."""
+        if not self._networks:
+            raw_data = self._digest()
+            for network in raw_data:
+                ipv4 = network.get("network", {}).get(IPV4, {})
+                ipv6 = network.get("network", {}).get(IPV6, {})
+
+                self._networks.append(NetworkDetails(
+                    name=network.get(NAME),
+                    mac=network.get(MAC_ADDRESS),
+
+                    address=ipv4.get(IP_ADDRESS, None),
+                    netmask=ipv4.get(NETMASK, None),
+                    gateway=ipv4.get(GATEWAY, None),
+                    broadcast=network.get(BROADCAST, None),
+                    dnsnameservers=ipv4.get(DNS, None),
+
+                    address6=ipv6.get(IP_ADDRESS, None),
+                    netmask6=ipv6.get(NETMASK, None),
+                    gateway6=ipv6.get(GATEWAY, None),
+                ))
+
+        return self._networks
 
 
 class NotExistingMetadataException(Exception):
@@ -139,6 +391,9 @@ class BaseMetadataService(object):
         """Get a list of space-stripped strings as public keys."""
         pass
 
+    def get_network_adapter(self):
+        pass
+
     def get_network_details(self):
         """Return a list of `NetworkDetails` objects.
 
@@ -146,6 +401,11 @@ class BaseMetadataService(object):
         network configuration, details which can be found
         in the namedtuple defined above.
         """
+        adapter = self.get_network_adapter()
+        if not adapter:
+            return
+
+        return adapter.get_network_details()
 
     def get_admin_password(self):
         pass
